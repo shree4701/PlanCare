942beeadd421e0625efb34fb9281c3f179dc4a8c
DESKTOP-9E0JAPF\shree <shree4701@gmail.com>
2025-01-22 09:55:24 +0800

    registration works

--- plancare.client/src/app/app.component.ts
+++ plancare.client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Car } from '../car.model';
@@ -10,38 +9,5 @@ import { CarSignalRService } from '../CarSignalRService';
   standalone: false,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  public cars: Car[] = [];
-  public filteredMake: string = ''; // Optional make filter
-
-  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute, private carSignalRService: CarSignalRService) { }
-
-  ngOnInit() {
-    // On initialization, get the query parameter 'make' if it exists
-    this.activatedRoute.queryParams.subscribe(params => {
-      this.filteredMake = params['make'] || ''; // Default to empty string if 'make' is not found
-      this.getCars(); // Load cars based on the filtered make
-    });
-  }
-
-  getCars() {
-
-    let params = new HttpParams();
-
-    // If 'make' is provided, add it as a query parameter
-    if (this.filteredMake) {
-      params = params.set('make', this.filteredMake);
-    }
-
-    this.http.get<Car[]>('/car', { params }).subscribe(
-      (result: Car[]) => {
-        this.cars = result;
-      },
-      (error: any) => {
-        console.error(error);
-      }
-    );
-  }
-
-  title = 'plancare.client';
+export class AppComponent {
 }
--- plancare.client/src/app/app.module.ts
+++ plancare.client/src/app/app.module.ts
@@ -32,10 +32,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
